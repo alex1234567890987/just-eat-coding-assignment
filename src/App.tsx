311b090dda24489c208563154ef5963581da6c5f
alex ganvir <alex7ganvir@gmail.com>
2026-03-31 16:19:22 +0100

    feat: fetch restaurant data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import './App.css'
+import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('...Loading');
+
+  useEffect(() => {
+    async function fetchRestaurants() {
+      try {
+        const response = await fetch('/api/discovery/uk/restaurants/enriched/bypostcode/EC4M7RF');
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch')
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+        setMessage('Data fetched, check console');
+      } catch (error) {
+        console.error('Something went wrong: ', error);
+        setMessage('Something went wrong while fetching the data');
+      }
+    }
+
+    fetchRestaurants()
+  }, [])
+
   return (
     <main>
       <h1>Just Eat Restaurant Finder</h1>
-      <p>Hello React + TypeScript</p>
+      <p>{message}</p>
     </main>
   )
 }
